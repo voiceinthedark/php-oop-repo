29e8cfd86bc3c1aa8326a2946418c7d150aa12fc
voiceinthedark <darkrisingforce@gmail.com>
2023-06-09 01:43:43 +0300

    added PDO prepared statements and execution

--- src/app/controllers/HomeController.php
+++ src/app/controllers/HomeController.php
@@ -8,22 +8,41 @@ use PDO;
 class HomeController
 {
 
-    public function index() : string
+    public function index(): string
     {
-        // open pdo connection to my_db database that runs on a docker container
-        $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', []);
+        try {
+
+            $id = 15;
+            // open pdo connection to my_db database that runs on a docker container
+            $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+            ]);
+            $query = 'SELECT * FROM users where id = ?';
+            $statment = $db->prepare($query);
+            $statment->execute([$id]);
+
+
+            foreach ($statment->fetchAll() as $row) {
+                echo "<pre>";
+                var_dump($row);
+                echo "</pre>";
+            }
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+        }
 
         return View::make('index', ['title' => 'Home']);
     }
 
 
 
-    public function upload() : void
+    public function upload(): void
     {
         move_uploaded_file($_FILES['file']['tmp_name'], STORAGE_PATH . '/' . $_FILES['file']['name']);
     }
 
-    public function download() : void{
+    public function download(): void
+    {
         header('Content-Type: application/pdf');
         header('Disposition: attachment; filename="invoice.pdf"');
 
