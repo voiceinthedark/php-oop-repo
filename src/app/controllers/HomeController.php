37f34036306f2f59e749c7f7460c018bc3b066bf
voiceinthedark <darkrisingforce@gmail.com>
2023-06-09 20:08:25 +0300

    added transactions

--- src/app/controllers/HomeController.php
+++ src/app/controllers/HomeController.php
@@ -10,25 +10,54 @@ class HomeController
 
     public function index(): string
     {
+        $id = 15;
         try {
 
-            $id = 15;
             // open pdo connection to my_db database that runs on a docker container
             $db = new PDO('mysql:host=db;dbname=my_db', 'root', 'root', [
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
             ]);
-            $query = 'SELECT * FROM users where id = ?';
-            $statment = $db->prepare($query);
-            $statment->execute([$id]);
-
-
-            foreach ($statment->fetchAll() as $row) {
-                echo "<pre>";
-                var_dump($row);
-                echo "</pre>";
-            }
         } catch (\PDOException $e) {
             echo $e->getMessage();
+        }
+
+        $name = 'Valentina Rossa';
+        $email = 'ValentinaRos@example.com';
+        $isactive = 1;
+        $createdat = date('Y-m-d H:i:s');
+        $amount = 863.75;
+
+        try {
+            $db->beginTransaction();
+
+            $insert_user_statement = 'INSERT INTO users (email, full_name, is_active, created_at) VALUES (:email, :full_name, :is_active, :created_at)';
+
+            $statment = $db->prepare($insert_user_statement);
+            $statment->execute(['email' => $email, 'full_name' => $name, 'is_active' => $isactive, 'created_at' => $createdat]);
+
+            $user_id = (int)$db->lastInsertId();
+
+            $insert_invoice_statement = 'INSERT INTO invoices (amount, user_id) VALUES (:amount, :user_id)';
+            $invoice_statement = $db->prepare($insert_invoice_statement);
+            $invoice_statement->execute(['user_id' => $user_id, 'amount' => $amount]);
+
+            $fetchStatement = $db->prepare('SELECT invoices.id AS invoice_id, amount, user_id, full_name FROM invoices
+            INNER JOIN users ON invoices.user_id = users.id
+            WHERE email = :email');
+            $fetchStatement->execute(['email' => $email]);
+
+            $db->commit();
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+            if ($db->inTransaction()) {
+                $db->rollBack();
+            }
+        }
+
+        foreach ($fetchStatement->fetch(PDO::FETCH_ASSOC) as $row) {
+            echo "<pre>";
+            var_dump($row);
+            echo "</pre>";
         }
 
         return View::make('index', ['title' => 'Home']);
